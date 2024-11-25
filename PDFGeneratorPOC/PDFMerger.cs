93f959d7d0d71f1b30f247ef07af54d9d3e15798
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-11-25 12:35:56 +0600

    projectAdded

---
+++ PDFGeneratorPOC/PDFMerger.cs
@@ -0,0 +1,30 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+
+namespace PDFGeneratorPOC
+{
+    public class PdfMerger
+    {
+        public void MergePdfs(string[] fileNames, string outputFileName)
+        {
+            using (PdfDocument outputDocument = new PdfDocument())
+            {
+                foreach (string file in fileNames)
+                {
+                    using (PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import))
+                    {
+                        int count = inputDocument.PageCount;
+                        for (int idx = 0; idx < count; idx++)
+                        {
+                            PdfPage page = inputDocument.Pages[idx];
+                            outputDocument.AddPage(page);
+                        }
+                    }
+                }
+                outputDocument.Save(outputFileName);
+            }
+        }
+    }
+
+}
---
+++ PDFGeneratorPOC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace PDFGeneratorPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                string[] inputFiles = { "PdfGenerator\\InputPdf\\Report_25_11_2024 11_44_30 am.pdf" ,"PdfGenerator\\InputPdf\\Report_25_11_2024 11_43_21 am.pdf"};
+                string outputFile = "PdfGenerator\\OutputPdf\\merged.pdf"; 
+                PdfMerger merger = new PdfMerger();
+                merger.MergePdfs(inputFiles, outputFile);
+                Console.WriteLine("PDFs merged successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("PDFs merged Failed!");
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+       
+        }
+    }
+}
